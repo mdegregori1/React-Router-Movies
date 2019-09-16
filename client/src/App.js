3e936b599f91b8d9f09cb48d4c253a3bb6f12ccf
Mauricio Degregori <degregori2@gmail.com>
2019-09-16 18:02:23 -0500

    1st half

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
 import React, { useState } from 'react';
 
 import SavedList from './Movies/SavedList';
+import Movie from "./Movies/Movie";
+import MovieList from "./Movies/MovieList";
+import { Route } from 'react-router-dom';
+import { BrowserRouter as Link } from "react-router-dom";
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
@@ -10,10 +14,16 @@ const App = () => {
   };
 
   return (
-    <div>
+    <div className = "Routes">
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <Route exact path = "/" component={MovieList}/>
+      <Route path = "/movies/id" component={Movie}/>
+      <Link exact path to = "/">{MovieList}</Link>
+      <Link exact path to = "/movies/id">{Movie}</Link>
+      //Not sure on these two, but notes for tomorrow ---> home should include all of the movies, but thats it 
+
     </div>
+ 
   );
 };
 
